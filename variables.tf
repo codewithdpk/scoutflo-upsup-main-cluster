61e0e960d311bdb6a57866248cbc49ab17fb5cf1
Rachit Sharma <rachitsharma615>
2024-04-01 06:18:43 +0530

    Initial Terraform

---
+++ variables.tf
@@ -0,0 +1,38 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "cluster_name" {
+  description = "Cluster name"
+  type = string
+  default = "scoutflo-test"
+}
+
+variable "instance_type" {
+  description = "Instance Type"
+  type = string
+  default = "t3a.large"
+}
+
+variable "min_size" {
+  description = "Min number of nodes"
+  type = number
+  default = 1
+}
+
+variable "max_size" {
+  description = "Max number of nodes"
+  type = number
+  default = 5
+}
+
+variable "desired_size" {
+  description = "Desired number of nodes"
+  type = number
+  default = 3
+}
